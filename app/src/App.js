45b815208f96fafa90ce52c018dc32c0220df85c
kotborealis <kotborealis@gmail.com>
2019-02-23 00:52:06 +0300

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import Seiran from 'seiran';
+import AceEditor from 'react-ace';
 
 class App extends Component {
     state = {
-        chunk: 'x = 10\n' +
-               'i = 0\n' +
-               '\n' +
-               'while i<=x do\n' +
-               '  local x = i*2    -- local to the while body\n' +
-               '  print(x)         --> 2, 4, 6, 8, ...\n' +
-               '  i = i + 1\n' +
-               'end\n' +
-               '\n' +
-               'print(sqrt(x))',
+        chunk: 'for i = 1, 100 do\n' +
+               '    if i % 15 == 0 then\n' +
+               '        print("FizzBuzz")\n' +
+               '    elseif i % 3 == 0 then\n' +
+               '        print("Fizz")\n' +
+               '    elseif i % 5 == 0 then\n' +
+               '        print("Buzz")\n' +
+               '    else\n' +
+               '        print(i)\n' +
+               '    end\n' +
+               'end',
         log: []
     };
 
@@ -31,14 +33,22 @@ class App extends Component {
         }));
     };
 
+    componentDidMount() {
+        this.run({preventDefault: () => 0});
+    }
+
     render() {
         const {chunk} = this.state;
         return (
             <div className="App">
                 <form onSubmit={this.run}>
-                    <textarea value={chunk}
-                              onChange={({target: {value}}) =>
-                                  this.setState({chunk: value})}
+                    <AceEditor
+                        value={chunk}
+                        width="100%"
+                        height="500px"
+                        mode="lua"
+                        theme="github"
+                        onChange={value => this.setState({chunk: value})}
                     />
                     <input type="submit" value="Submit"/>
                     <pre>
