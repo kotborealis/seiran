21d9aa903c25c84e44b106835dc0adcecc4436f4
kotborealis <kotborealis@gmail.com>
2019-02-24 00:38:53 +0300

    Updates

---
+++ precache-manifest.2dd81b670213060b88ae3ec8e362569e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c04de6c4d5872b7623d",
+    "url": "/seiran/static/js/runtime~main.5c04de6c.js"
+  },
+  {
+    "revision": "8b5e992a443d1f44ca5f",
+    "url": "/seiran/static/js/main.8b5e992a.chunk.js"
+  },
+  {
+    "revision": "4aa6323dd01e5e3abb11",
+    "url": "/seiran/static/js/2.4aa6323d.chunk.js"
+  },
+  {
+    "revision": "8b5e992a443d1f44ca5f",
+    "url": "/seiran/static/css/main.f1825e0b.chunk.css"
+  },
+  {
+    "revision": "3b876cfdba051d06dc6c4aadd88d318b",
+    "url": "/seiran/index.html"
+  }
+];
